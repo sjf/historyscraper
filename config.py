e7294cf8ca8120d69d67b8378563adc7ac63968f
sjf <sarah.fortune@gmail.com>
2019-08-01 11:02:04 -0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import auth
+URL = "https://start.interviewing.io/api/interviews"
+
+QUERY_PARAMS = {"after":""}
+
+HEADERS = {
+    'cookie': auth.cookie,
+    'authorization': auth.bearer,
+    'authority': "start.interviewing.io",
+    'accept': "application/json, text/plain, */*",
+    'cache-control': "no-cache",
+    'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+    'referer': "https://start.interviewing.io/history",
+    'Host': "start.interviewing.io",
+    'Connection': "keep-alive",
+    'pragma': "no-cache",
+}
+
+# curl 'https://start.interviewing.io/history'
+# -H 'upgrade-insecure-requests: 1'
+# -H 'dnt: 1'
+# -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3'
+# -H 'referer: https://start.interviewing.io/'
+# -H 'accept-encoding: gzip, deflate, br'
+# -H 'accept-language: en-US,en;q=0.9,az-AZ;q=0.8,az;q=0.7,fa-IR;q=0.6,fa;q=0.5'
+# -H 'cookie: XXX
---
+++ main.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+import sys
+import config
+from datetime import datetime
+import requests
+from requests.exceptions import RequestException
+from contextlib import closing
+import jsonpath_rw
+import re
+import json
+
+def log(*args):
+  date = datetime.now().strftime("%Y-%m-%d %H:%M:%S ")
+  print(date + " ".join(map(str, args)))
+
+def get(url, headers, query_params):
+  """ Returns the contents of the URL. If there is an error it is logged and None is returned. """
+  try:
+    log("Making request to {0}".format(url))
+    with closing(requests.request("GET", url, headers=headers, params=query_params)) as response:
+      if not is_ok(response):
+        log("Request failed, code: {0} {1}".format(response.status_code, response.headers))
+        return None
+      log("Request successful, code: {0}".format(response.status_code))
+      return response.content
+  except RequestException as e:
+    log("Request for '{0}' unsuccessful: {1}".format(url, str(e)))
+    return None
+
+def is_ok(response):
+  content_type = response.headers['Content-Type'].lower()
+  return (response.status_code == 200
+          and content_type is not None)
+
+def get_all():
+  result = {'interviews':[]}
+  params = {}
+  after = ""
+  mx = 10
+  while after is not None and mx >= 0:
+    if after:
+      params['after'] = after
+
+    res = get(config.URL, config.HEADERS, params)
+    if not res:
+      break
+    content = json.loads(res)
+
+    after = content['nextAfter']
+    interviews = content['interviews']
+    print("total count: %d, next after: %s, # interviews: %d" % (content['totalCount'],
+      after, len(interviews)))
+    print()
+    result['interviews'].extend(content['interviews'])
+    mx -= 1
+  # remove last two interviews
+  result['interviews'] = result['interviews'][:-2]
+  return result
+
+def save(data):
+  with open('history.json', 'w') as outfile:
+    json.dump(data, outfile)
+
+def summary(data):
+  summaries = [match.value for match in jsonpath_rw.parse('$.interviews[*].interviewer.review.summary').find(data)]
+  with open('history.txt', 'w') as outfile:
+    for summary in summaries:
+      outfile.write(summary)
+      outfile.write('----------------------------------------------------')
+  print("Saved feedback for %d interviews" % len(summaries))
+
+def main():
+  history = get_all()
+  save(history)
+  summary(history)
+
+if __name__ == '__main__':
+  main()
